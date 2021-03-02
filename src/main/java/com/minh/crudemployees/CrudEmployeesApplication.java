a613e49e0b9448e2357845c5283a83d7b24c663b
minh <minh@minhs-MacBook-Pro.local>
2021-03-03 04:39:35 +0700

    Create backend crud

---
+++ src/main/java/com/minh/crudemployees/CrudEmployeesApplication.java
@@ -0,0 +1,13 @@
+package com.minh.crudemployees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudEmployeesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudEmployeesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/minh/crudemployees/CrudEmployeesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minh.crudemployees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudEmployeesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
